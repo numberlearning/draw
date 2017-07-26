826ce75801e89ed00b2b40df063615d28b3335a4
Mengting Fang <121mengting@163.com>
2017-07-26 16:20:36 -0700

    Update draw_eric_rewrite_filterbank.py

--- draw_eric_rewrite_filterbank.py
+++ draw_eric_rewrite_filterbank.py
@@ -89,7 +89,7 @@ def filterbank(gx, gy, sigma2, delta, N):
 
 
 def attn_window(scope,h_dec,N):
-    with tf.variable_scope(scope,reuse=REUSE):
+    with tf.variable_scope(scope,reuse=DO_SHARE):
         params=linear(h_dec,3+2*N)
     split=tf.split(params, 3+2*N, 1)
     gx_=split[0]
